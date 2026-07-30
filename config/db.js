18fe42887f16b560b51f7518f011a1196c9b7018
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-07-30 16:47:59 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -11,9 +11,12 @@ const __dirname = path.dirname(__filename);
 // Step up one level to reach the root directory where pg-ca.pem is located
 const pgCaPath = path.join(__dirname, '..', 'pg-ca.pem');
 
-const { Pool } = pg;
+//const { Pool } = pg;
 
-export const pool = new Pool({
+//export const pool = new Pool({
+// ....logic here...
+//});
+export const pool = {
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
@@ -24,4 +27,4 @@ export const pool = new Pool({
     ca: fs.readFileSync(pgCaPath).toString(),
   },
   connectionTimeoutMillis: 5000, // Fail quickly (5s) instead of hanging indefinitely
-});
+};
