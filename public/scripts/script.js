515122a247027264c8ec05a088d17d3edeff0899
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-01 12:33:58 +0100

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,54 @@
+const authSection = document.getElementById("auth-section");
+const BACKEND_URL = "";
 
+    async function checkAuthStatus() {
+      try {
+        // 'credentials: include' forces the browser to send the session cookie
+        const response = await fetch(`${BACKEND_URL}/api/auth/user`, { credentials: 'include' });
+        data = await response.json();
+ 
+        
+        if (data.loggedIn) {
+          // User is authenticated! Display details saved from Database 
+         authSection.innerHTML = `
+         <div id="closer"  ><i class="fa-solid fa-xmark" onclick="closeAuth()"></i></div>
+         <div class="user-details">
+         <div class="user-dp">
+         <img src="${data.user.profilePic}" width="55" height="55" style="border-radius:50%">
+         </div>
+         <div class="other-info">
+           <h2>${data.user.displayName}</h2>
+           <p id="user-email">${data.user.email}</p>
+           <div class="amount-download">
+           <p id="entries-amount">${data.user.entries.length} Entries</p>
+           <a href="/user/download-txt">Download my data </a>
+           </div>
+           </div>
+           </div>
+           
+           <button onclick="logoutUser()">Log Out</button>
+         `;
+         //   authSection.innerHTML = '<h1>User Logged In</h2>';
+        //    toggleCtrl.classList.remove('hidden');
+          //  searchIcon.classList.remove('hidden');
+       //     signInBtn.classList.add('hidden');
+        //    userDP.classList.remove('hidden');
+         //   userDP.src = data.user.profilePic;
+        //    getEntries(displayEntries);  
+        } else {
+          // User cookie expired or doesn't exist
+         //   userDP.classList.add('hidden');
+         //   signInBtn.classList.remove('hidden');
+          authSection.innerHTML = `
+          <div id="closer"><i class="fa-solid fa-xmark" onclick="closeAuth()"></i></div>
+            <h2>Please sign up or log in</h2>
+            <a href="${BACKEND_URL}/auth/google"><button>Sign In with Google</button></a>
+          `;
+            //other logics
+        }
+      } catch (err) {
+        console.error("Error verifying authentication status:", err);
+      }
+              }
+
+function closeAuth(){}
