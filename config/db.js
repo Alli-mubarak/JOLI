0f013961f674190e086f2c3ba4c70f49cd1ed379
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-24 09:24:06 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,6 +5,8 @@ import path from 'path';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 
+const globalForPg = global;
+
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,9 +14,7 @@ const __dirname = path.dirname(__filename);
 // Step up one level to reach the root directory where pg-ca.pem is located
 const pgCaPath = path.join(__dirname, '..', 'pg-ca.pem');
 
-const { Pool } = pg;
-
-export const pool = new Pool({
+  const dbConfig = {
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   user: process.env.DB_USER,
@@ -24,7 +24,12 @@ export const pool = new Pool({
     rejectUnauthorized: true,
     ca: fs.readFileSync(pgCaPath).toString(),
   },
-  max: 3,                 // Tight limit for free tier
+  max: 1,                 // Tight limit for free tier
   idleTimeoutMillis: 5000,
   connectionTimeoutMillis: 20000, // Fail quickly (20s) instead of hanging indefinitely
-});
+};
+
+export const pool = globalForPg.pgPool || new pg.Pool(dbConfig);
+
+if (process.env.NODE_ENV !== 'production') globalForPg.pgPool = pool;
+  
