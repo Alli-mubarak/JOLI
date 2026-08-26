c7174dd59cb81f4f660ea5e8366adaa72a1841ff
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-26 09:09:21 +0100

    Implement fetchPosts function to retrieve posts
    Added async function to fetch posts from API and handle errors.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,2 +1,17 @@
 const postsContainer = document.getElementById("posts");
 postsContainer.innerHTML = `<pre>Fetching posts....</pre>`;
+
+async function fetchPosts() {
+      try {
+        
+        const response = await fetch("/api/getPosts" );
+        posts = await response.json();
+ 
+        
+        console.log(posts);
+      } catch (err) {
+        console.error("Error fetching posts:", err);
+      }
+}
+
+fetchPosts()
