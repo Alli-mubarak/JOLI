b1f53075e6937957793c2f59ad6381c1d510475c
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-27 11:32:43 +0100

    Add bold tags to time difference output

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -89,15 +89,15 @@ const dDifference = msDifference / (1000 * 60 * 60 * 24);
 const mtDifference = msDifference / (1000 * 60 * 60 * 24 * 12);
 
 if(mtDifference > 1){
-return Math.floor(mtDifference) + "M"; 
+return Math.floor(mtDifference) + "<bold>M</bold>"; 
 }else if (dDifference > 1){
-return Math.floor(dDifference) + "d";
+return Math.floor(dDifference) + "<bold>d</bold>";
 }else if (hDifference > 1){
-return Math.floor(hDifference) + "h";
+return Math.floor(hDifference) + "<bold>h</bold>";
 }else if (mDifference > 1){
-return Math.floor(mDifference) + "m";
+return Math.floor(mDifference) + "<bold>m</bold>";
 }else {
-return Math.floor(sDifference) + "s";
+return Math.floor(sDifference) + "<bold>s</bold>";
 }
 
 }
