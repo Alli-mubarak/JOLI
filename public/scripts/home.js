50b0970e62d68775cc472beff8dc42466b97139b
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-28 15:29:37 +0100

    Log post type and adjust fetchPosts timing

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -9,6 +9,7 @@ async function fetchPosts() {
         const data = await response.json();
  
         const posts = data.posts
+      console.log(typeof(posts));
       postsContainer.innerHTML = '';
             
       for (let i=posts.length-1; i >=0; i--){
@@ -123,7 +124,7 @@ return result;
 }
 
 
-//setTimeout(() =>{
+setTimeout(() =>{
 fetchPosts()
-//},4000);
+},4000);
 
