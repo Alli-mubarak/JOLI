d56d69a54b143a7e56c695ef9f94494db43449a5
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-28 09:06:33 +0100

    Remove scroll event listener for header and tabs
    Removed unused page header and tabs scroll functionality.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,6 +1,5 @@
 const postsContainer = document.getElementById("posts");
-const pageHeader = document.querySelector("header");
-const pageTabs = document.querySelector(".tabs");
+
 
 
 async function fetchPosts() {
@@ -122,25 +121,7 @@ function sortImages(images){
        });
 return result;
 }
-let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
-window.addEventListener('scroll', function() {
-try{
- let currentScroll = window.scrollY || document.documentElement.scrollTop;
 
- if (currentScroll > lastScrollTop) {
-       pageHeader.style.top = "-100px";
-       pageTabs.style.bottom = "-100px";
- } else if (currentScroll < lastScrollTop) {
-       pageHeader.style.top = 0;
-       pageTabs.style.bottom = 0;
- }
-
-       lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
-}catch(e){
-      console.error(e);
-      return;
-}
- }, { passive: true }); 
 
 //setTimeout(() =>{
 fetchPosts()
