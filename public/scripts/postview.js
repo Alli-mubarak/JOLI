85cbd8e9b2ddb8cb0827ac00df38d4bccb3534cc
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-08-30 12:19:04 +0100

    Create postview.js

---
+++ public/scripts/postview.js
@@ -0,0 +1,111 @@
+const mediaViewer = document.getElementById("media-viewer");
+  const mediaViewerCloser = document.getElementById("mv-closer");
+  const moveLeft = document.getElementById("mv-left");
+  const moveRight = document.getElementById("mv-right");
+  const viewer = document.getElementById("viewer");
+  const mediaCounter = document.getElementById("media-counter");
+  const imageBox = document.getElementById("image-view");
+  let imgArray;
+  let currIndex;
+  let inViewMode = false;
+    
+function viewPostImage(e){
+   try{
+   inViewMode = true;
+    const img = e.target;
+    const src = img.src
+    imgArray = Array.from(img.parentElement.children);
+   
+   const index = imgArray.indexOf(img);
+   currIndex = index;
+     if(imgArray.length > 1){
+         imgArray.forEach((c) =>{
+             mediaCounter.innerHTML += `<div></div>`
+         });
+         mediaCounter.children[index].style.background = "#fff";
+         
+     }
+         imageBox.src = src;
+         if(index === 0){
+             moveLeft.classList.add("hidden");
+         }
+         if(index === imgArray.length -1){
+             moveRight.classList.add("hidden");
+         }
+     
+        mediaViewer.classList.remove("hidden");
+      
+      
+       }catch(e){
+           console.error(e);
+       }     
+        }
+        
+    mediaViewerCloser.onclick = (e) =>{
+    try{
+        mediaViewer.classList.add("hidden");
+        mediaCounter.innerHTML = "";
+        inViewMode = false;
+        moveLeft.classList.remove("hidden");
+        moveRight.classList.remove("hidden");
+        imgArray = "";
+       }catch(e){
+           console.error(e);
+       }
+    }
+    
+    moveRight.onclick = () =>{
+    try{
+        if(inViewMode){
+        if(currIndex  < imgArray.length - 1){
+         
+        imageBox.src = imgArray[currIndex + 1].src;
+       const mcArray = Array.from(mediaCounter.children);
+       mcArray.forEach(c=>{
+           c.style.background = "transparent";
+       })
+       mediaCounter.children[currIndex + 1].style.background = "#fff";
+       
+       moveLeft.classList.remove("hidden");
+       
+       
+       if(currIndex+2 === imgArray.length){
+             moveRight.classList.add("hidden");   
+         }
+         currIndex += 1;
+       }else{
+           moveRight.classList.add("hidden");   
+       }
+        }
+     }catch(e){
+           console.error(e);
+       }
+    }
+    
+    moveLeft.onclick = () =>{
+    try{
+        if(inViewMode){
+        if(currIndex  !== 0){
+         
+        imageBox.src = imgArray[currIndex - 1].src;
+       const mcArray = Array.from(mediaCounter.children);
+       mcArray.forEach(c=>{
+           c.style.background = "transparent";
+       })
+       mediaCounter.children[currIndex - 1].style.background = "#fff";
+       
+       moveRight.classList.remove("hidden");
+       
+      
+       if(currIndex === 1){
+             moveLeft.classList.add("hidden");   
+         }
+        currIndex -= 1;
+       }else{
+           moveLeft.classList.add("hidden");   
+       }
+        }
+     }catch(e){
+           console.error(e);
+       }
+           }
