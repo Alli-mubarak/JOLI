942438ebf8698c851afce8bc363d5d567dce4c28
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-09 13:04:14 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -136,14 +136,14 @@ const initDb = async () => {
     )
 );
    CREATE TABLE IF NOT exists likes (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT unique_user_post_like UNIQUE (user_id, post_id)
 );
 CREATE TABLE IF NOT exists comments (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE, 
     content TEXT NOT NULL,                                      
