be183f381bf815c5d363857adc5507adaa7c24e9
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-11 11:47:20 +0100

    Update friends.js

--- public/scripts/friends.js
+++ public/scripts/friends.js
@@ -30,7 +30,7 @@ try{
 const userCard = `
 <div class="user">
     <div class="pp-box">
-       <img src=${user.profile_picture} class="user-pic" alt="user picture" />
+       <img src=${user.profile_picture || "/images/default-user.png"} class="user-pic" alt="user picture" />
     </div>
     <div class="user-details">
        <button class="add-friend-btn"><i class="fa-solid fa-plus"></i> Add </button>
