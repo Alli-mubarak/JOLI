5b8011cff6cd48d99bb2e40734dc7bb7213fa793
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-11 11:18:22 +0100

    Create friends.js

---
+++ public/scripts/friends.js
@@ -0,0 +1,17 @@
+const message = "hi"
+
+const user = `
+ <div class="user">
+    <div class="pp-box">
+       <img src="user.png" class="user-pic" alt="user picture" />
+    </div>
+    <div class="user-details">
+       <button class="add-friend-btn"><i class="fa-solid fa-plus"></i> Add </button>
+       <div class="username-bio">
+          <p class="username">user</p>
+          <small class="bio">Happy to be here</small>
+       </div>
+     </div>
+ </div>
+           
+`;
