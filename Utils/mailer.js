b935857b4c0a216501c009c1b180f57655363261
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-12 13:45:51 +0100

    Update mailer.js

--- Utils/mailer.js
+++ Utils/mailer.js
@@ -1,1 +1,16 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
+// Create the transporter
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+
+export default transporter
+      
