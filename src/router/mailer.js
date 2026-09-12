085791b40587a4993c3f934a500b43469558cbbd
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-12 15:51:48 +0100

    Create mailer.js

---
+++ src/router/mailer.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/mails', (req, res) => {
+  try{
+  res.json({ message: 'Fetching all mails' });
+  }catch(err){
+    res.json({ message: 'Error fetching all mails', error: err });
+  }
+});
+
+
+module.exports = router;
+    
+export default router;
