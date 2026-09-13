9c154650d7dc4a1b90480c1382aa714c1d7b8d9a
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-13 08:44:13 +0100

    Comment out authRoutes middleware
    Commented out the authRoutes middleware in app.js.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {pool} from '../config/db.js'; 
 import mailRoutes from './router/mailer.js'; 
-import {authRoutes} from './router/auth.js'; 
+import authRoutes from './router/auth.js'; 
 import connectPgSimple from 'connect-pg-simple';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
@@ -435,7 +435,7 @@ async function fetchAuthorDetails(authorId){
 
 //***"""""""""""
 app.use('/api/m', mailRoutes);
-app.use('/api/auth', authRoutes);
+//app.use('/api/auth', authRoutes);
 //***********
 // function for detecting post like
 async function getPostLikeStatus(postId, userId){
