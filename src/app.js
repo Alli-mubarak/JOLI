e083feee5843dba3bc140f704b165551c6e37188
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-13 10:14:02 +0100

    Remove old post fetching APIs and update postRoutes import
    Removed the old post fetching APIs and updated the import path for postRoutes.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {pool, initDb} from '../config/db.js'; 
 import mailRoutes from './router/mailer.js'; 
 import authRoutes from './router/auth.js'; 
-import postRoutes from './router/post.js'; 
+import postRoutes from './router/posts.js'; 
 import connectPgSimple from 'connect-pg-simple';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
@@ -472,89 +472,6 @@ const queryText = `
     return res.status(500).json({ error: 'Internal server error' });
   }
 });
-
-//api for fetching posts
-app.get('/api/getPosts',async(req, res)=>{
-console.log('posts fetched initially \n');
-try{
-const result = await pool.query('SELECT * FROM posts ORDER BY created_at DESC LIMIT 30');
-const posts = result.rows
-for(let i = 0; i < posts.length; i++){
-  let author = await fetchAuthorDetails(posts[i].user_id);
-  posts[i].author_username = author.username;
-  posts[i].author_is_verified = author.is_verified;
-  posts[i].author_is_active = author.is_active;
-  posts[i].author_profile_picture = author.profile_pic;
-  author = [];
-  if(req.user && req.user.id){
-    const likeStat = await getPostLikeStatus(posts[i].id, req.user.id);
-    posts[i].likeStatus = likeStat
-  }
-  const commentsQuery = `
-      SELECT c.*, 
-      u.username AS commenter,
-      u.profile_picture AS commenter_pic
-      FROM comments c 
-      JOIN users u ON c.user_id = u.id 
-      WHERE c.post_id = $1 
-      ORDER BY c.created_at DESC
-    `;
-    const commentsResult = await pool.query(commentsQuery, [posts[i].id]);
-    posts[i].comments = commentsResult.rows;
-  
-}
-res.status(200).json({posts: posts});
-}catch(e){
-  console.error('Error fetching posts:', e);
-  res.status(500).json({error: 'Internal Server Error'});
-}
-});
-
-  //api for fetching more posts
-app.post('/api/getPosts',async(req, res)=>{
-console.log('more posts fetched \n');
-const {time} = req.body;
-  console.log(time);
-if(!time){
-  return res.status(400).json({ error: 'a specific time is required' });
-}
-try{
-const result = await pool.query(`SELECT * FROM posts WHERE created_at < '${time}'::timestamptz LIMIT 30`);
-const posts = result.rows
-if(posts.length < 1){
-  return res.status(404).json({ message: 'No more posts to fetch' });
-}
-for(let i = 0; i < posts.length; i++){
-  let author = await fetchAuthorDetails(posts[i].user_id);
-  posts[i].author_username = author.username;
-  posts[i].author_is_verified = author.is_verified;
-  posts[i].author_is_active = author.is_active;
-  posts[i].author_profile_picture = author.profile_pic;
-  author = [];
-  if(req.user && req.user.id){
-    const likeStat = await getPostLikeStatus(posts[i].id, req.user.id);
-    posts[i].likeStatus = likeStat
-  }
-  const commentsQuery = `
-      SELECT c.*, 
-      u.username AS commenter,
-      u.profile_picture AS commenter_pic
-      FROM comments c 
-      JOIN users u ON c.user_id = u.id 
-      WHERE c.post_id = $1 
-      ORDER BY c.created_at DESC
-    `;
-    const commentsResult = await pool.query(commentsQuery, [posts[i].id]);
-    posts[i].comments = commentsResult.rows;
-  
-}
-res.status(200).json({posts: posts});
-}catch(e){
-  console.error('Error fetching posts:', e);
-  res.status(500).json({error: 'Internal Server Error'});
-}
-});
-
 
 
 //post liking api
