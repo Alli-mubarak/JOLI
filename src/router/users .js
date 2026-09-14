b71134c8487561d5a153185849e7dc783955ff75
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-14 16:42:56 +0100

    Update users .js

--- src/router/users .js
+++ src/router/users .js
@@ -1,1 +1,54 @@
-const message = "Hello user";
+import express from 'express'; 
+import geoip from "geoip-lite";
+import {pool} from '../../config/db.js'; 
+import path from 'node:path';
+const router = express.Router();
+const __dirname = import.meta.dirname;
+import { v2 as cloudinary } from 'cloudinary';
+import 'dotenv/config'; // Automatically loads environment variables
+import 'ejs';
+import transporter from './mailer.js';
+
+// cloudinary configuration
+cloudinary.config({ 
+        cloud_name: process.env.CLOUD_NAME, 
+        api_key: process.env.CLOUD_API_KEY, 
+        api_secret: process.env.CLOUD_API_SECRET 
+    });
+
+function getCountryNameFromReq(req) {
+  // Extract client IP address from request header
+  const clientIp = req.headers['x-forwarded-for']
+  // Lookup geolocation data using geoip-lite
+  const geo = geoip.lookup(clientIp);
+  let countryName = 'Unknown';
+  if (geo && geo.country) {
+    try {
+      // Convert the 2-letter code (e.g., 'US') to full name (e.g., 'United States')
+      countryName = countryNamesInEnglish.of(geo.country);
+    } catch (error) {
+      // Fallback to the country code if the lookup fails for any reason
+      countryName = geo.country;
+    }
+    return countryName;
+  }
+}
+
+//session checker
+const checkSession = (req, res, next) => {
+  try{
+    if (req.session) {
+        next(); 
+    } else {
+        console.error("Unauthorized usage");
+        res.status(401).json({ error: 'You must be logged in to do this' });
+       
+    }
+  }catch(e){
+    console.error(e);
+  }
+};
+
+
+
+export default router;
