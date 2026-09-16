13c415879dcbc3e97aa2ba57020582a39a55c582
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-16 06:44:58 +0100

    Convert reset-password route to async function

--- src/router/auth.js
+++ src/router/auth.js
@@ -588,7 +588,7 @@ authRouter.get('/logout', checkSession, limiter, async(req, res) => {
   }
 });
 
-authRouter.post('/reset-password', limiter, (req, res) => {
+authRouter.post('/reset-password', limiter, async(req, res) => {
   try{
   const {email} = req.body;
   if(req.isAuthenticated() || req.user) {
