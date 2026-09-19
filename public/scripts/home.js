bf04981e7e7e8665b1d3db234fc0480438469fab
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-19 12:10:02 +0100

    Add functionality to view author and add friend

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -452,7 +452,42 @@ async function viewPostMenu(e){
     const delBtn = postMenu.querySelector("#delete-post-btn");
 
     
+if(addFriendBtn){
+      addFriendBtn.onclick = async() =>{
+        try{
+       await addFriend(authorId);
+      
+        postMenuCloser.style.background = "transparent";
+    
+     setTimeout(() =>{
+      postMenuContainer.style.bottom = "-100vh";
+      },200);
+   enableScrolling();
+        }
+        catch(err){
+          notify("failed to send request!", "error");
+          return 
+        }
+      }
+    }
 
+    if(viewAuthorBtn){
+      viewAuthorBtn.onclick = () =>{
+        try{
+       window.location.href = authorLink;
+        postMenuCloser.style.background = "transparent";
+    
+     setTimeout(() =>{
+      postMenuContainer.style.bottom = "-100vh";
+      },200);
+   enableScrolling();
+        }
+        catch(err){
+          notify("Author profile view failed!", "error");
+          return 
+        }
+      }
+    }
     
     if(delBtn){
       delBtn.onclick = () =>{
