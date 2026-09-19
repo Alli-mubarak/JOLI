2bdf62b399e2260e1e2c585a12fdb650e15925de
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-19 10:01:03 +0100

    Fix variable name for user friends query

--- src/router/users.js
+++ src/router/users.js
@@ -62,7 +62,7 @@ const checkSession = (req, res, next) => {
 
 async function fetchUserFriends(id){
   try{
-    const frQuery = "SELECT * FROM friendships WHERE (sender_id = $1 OR receiver_id = $1)"
+    const query = "SELECT * FROM friendships WHERE (sender_id = $1 OR receiver_id = $1)"
     const result = await pool.query(query, [id]);
  //   const frQuery = await pool.query(`SELECT * FROM friendships WHERE ($1 IN (sender_id, receiver_id))`, [id]);
     console.log(result);
