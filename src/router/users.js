2b39d1aefc4496eb74b2d3b705cf154fb9f98614
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-19 09:55:47 +0100

    Refactor SQL query to use IN clause for friendships

--- src/router/users.js
+++ src/router/users.js
@@ -62,7 +62,7 @@ const checkSession = (req, res, next) => {
 
 async function fetchUserFriends(id){
   try{
-    const frQuery = await pool.query(`SELECT * FROM friendships WHERE (sender_id = $1 OR receiver_id = $1) `, [id]);
+    const frQuery = await pool.query(`SELECT * FROM friendships WHERE ($1 IN (sender_id, receiver_id))`, [id]);
     console.log(frQuery);
     return {
       count: frQuery.rows.length,
