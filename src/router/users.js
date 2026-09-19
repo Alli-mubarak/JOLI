d080cf1f03da15aec38bc361a086debbebc16400
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-19 09:38:06 +0100

    Refactor SQL query string syntax in fetchUserFriends

--- src/router/users.js
+++ src/router/users.js
@@ -62,7 +62,7 @@ const checkSession = (req, res, next) => {
 
 async function fetchUserFriends(id){
   try{
-    const frQuery = await pool.query('SELECT * FROM friendships WHERE (sender_id = $1 OR receiver_id = $1) AND status = $2', [id, "accepted"]);
+    const frQuery = await pool.query(`SELECT * FROM friendships WHERE (sender_id = $1 OR receiver_id = $1) AND status = 'accepted'`, [id]);
     console.log(frQuery);
     return {
       count: frQuery.rows.length,
