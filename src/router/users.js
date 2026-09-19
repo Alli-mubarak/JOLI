94165980dabd85e451d040d6f872f1a3ee11eb40
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-19 09:58:52 +0100

    Fix SQL query for fetching user friends

--- src/router/users.js
+++ src/router/users.js
@@ -62,11 +62,13 @@ const checkSession = (req, res, next) => {
 
 async function fetchUserFriends(id){
   try{
-    const frQuery = await pool.query(`SELECT * FROM friendships WHERE ($1 IN (sender_id, receiver_id))`, [id]);
-    console.log(frQuery);
+    const frQuery = "SELECT * FROM friendships WHERE (sender_id = $1 OR receiver_id = $1)"
+    const result = await pool.query(query, [id]);
+ //   const frQuery = await pool.query(`SELECT * FROM friendships WHERE ($1 IN (sender_id, receiver_id))`, [id]);
+    console.log(result);
     return {
-      count: frQuery.rows.length,
-      friends : frQuery.rows
+      count: result.rows.length,
+      friends : result.rows
     }
               
   }
