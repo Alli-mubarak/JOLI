4d719fba84a453fe872e6a597631a2c7a4ed91a9
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-20 09:19:56 +0100

    Add alert messages in userview.js

--- public/scripts/userview.js
+++ public/scripts/userview.js
@@ -1,5 +1,5 @@
 const postCards = document.querySelectorAll(".postCard");
-
+alert("hello");
 if(postCards && poscCards.children.length > 0){
   Array.from(postCards.children).forEach(p => {
     alert("yeah");
