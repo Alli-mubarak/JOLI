f68b624d1a043fdc283c7b5b9fcec743d9124986
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-20 07:04:14 +0100

    Change posts count format in user data

--- src/router/users.js
+++ src/router/users.js
@@ -118,7 +118,7 @@ let friends = await fetchUserFriends(userData.id);
   if(posts.error){
     return res.status(500).send("Could not fetch posts");
   }
-  if(posts.count > 0) userData.posts_count = `{${posts.count}}`;
+  if(posts.count > 0) userData.posts_count = `  (${posts.count})`;
   
   if(req.user && req.user.id){
     if(userData.username === req.user.username){
