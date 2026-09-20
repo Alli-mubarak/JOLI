88d66e1d0b1eb4f75861f53f43d4cfdeed5bb660
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-20 08:36:49 +0100

    Add userview.js to handle post card interactions

---
+++ public/scripts/userview.js
@@ -0,0 +1,7 @@
+const postCards = document.querySelectorAll(".postCard");
+
+if(postCards && poscCards.children.length > 0){
+  Array.from(postCards.children).forEach(p => {
+    alert("yeah");
+  })
+}
