92800056e6a8a22d2ebb576fd53d9b83379dfbd7
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-21 22:42:02 +0100

    Update users.js

--- src/router/users.js
+++ src/router/users.js
@@ -91,7 +91,13 @@ let friends = await fetchUserFriends(userData.id);
   if(friends.error){
     return res.status(500).send("Could not fetch friends");
   }
-  
+if(req.isAuthenticated() && req.user && userData.username !== req.user.username ) {  
+  const isFriend = friends.friends.some(f => f.sender_id === req.user.id || f.receiver_id === req.user.id);
+  userData.is_friend = isFriend;
+}
+  const hasAdmin = friends.some(f => f.sender_id === "Admin");
+console.log(hasAdmin); // Output: true
+      
   if(friends.count < 2){ userData.friends = friends.count + " friend"}
   else{userData.friends = friends.count + " friends"}
   
