83cb7f83c9cc275a70c41829f6aee786dfa72cae
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-22 11:07:38 +0100

    Remove authorization check in sortFriendship function
    Remove authorization check before sorting friends.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -18,7 +18,6 @@ let postOnFocus, lastPostCreationTime, friends, pendings;
 
 async function sortFriendship(){
   alert("sorting friends!");
-  if(!isAuthorised) return;
   alert(friendships);
   const friends = await friendships.filter(f => f.status === "accepted");
   const pendings  = await friendships.filter(f => f.status === "pending");
