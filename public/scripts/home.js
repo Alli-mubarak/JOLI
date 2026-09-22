e5222db9b23d941a59617d41d26deaec11651c7f
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-22 11:20:05 +0100

    Restore sortFriendship call in viewPost function
    Reintroduced sortFriendship function call in viewPost function.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -25,7 +25,7 @@ async function sortFriendship(){
     alert(pendings);
   
 }
-sortFriendship();
+
 
 function linkify(text) {
   const urlRegex = /(https?:\/\/[^\s]+|www\.[^\s]+)/g;
@@ -228,7 +228,7 @@ everyPosts.forEach((post, index) => {
 //function that makes the post card work
 function viewPost(e){
 try{
-  alert(friendships);
+  sortFriendship();
       
       if(e.target.getAttribute('data-type') !== null || e.target.parentElement.getAttribute('data-type') !== null){
         if(e.target.getAttribute('data-type') === "like" || e.target.parentElement.getAttribute('data-type') === "like"){
