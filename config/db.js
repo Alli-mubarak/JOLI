3132b27cc17fc472ce971fd91c26eaddfe650c4e
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-23 11:20:54 +0100

    Add messages table to database schema

--- config/db.js
+++ config/db.js
@@ -130,7 +130,15 @@ CREATE TABLE IF NOT exists password_resets (
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
-
+CREATE TABLE IF NOT exists messages (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    sender_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    receiver_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_read BOOLEAN DEFAULT FALSE
+);
   `;
   try {
     await pool.query(setupScript);
