94f115db661275fa90259176cd16f9a4b3859063
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-23 08:40:52 +0100

    Correct user picture fallback in comment form
    Fix user picture source in comment form.

--- public/scripts/userview.js
+++ public/scripts/userview.js
@@ -541,7 +541,7 @@ const commentFormNCloser = `
 <i class="fa-solid fa-xmark" id="u-closer-btn"></i>
 <form id="comment-form">
 <div class="pic-submit">
-<img src=${userPic || '/images/default-user'} alt="user picture" id="commenter-pic"/>
+<img src=${userPic || '/images/default-user.png'} alt="user picture" id="commenter-pic"/>
 <button type="submit" id="submit-comment-btn" disabled>
 <span>Post</span>
   <div id="c-loader" class="hidden"></div>
