0412fdec023146b983be5ba5ebf6d81c96284411
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-23 14:16:00 +0100

    Update messages.js

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,15 +1,4 @@
 window.onload = function() {
-
-  import { Server } from "socket.io";
-
-const io = new Server(3000, {
-  pingInterval: 300,
-  pingTimeout: 200,
-  maxPayload: 1000000,
-  cors: {
-    origin: "https://joli-indol.vercel.app"
-  }
-});
   
   async function run(){
    async function go(){
