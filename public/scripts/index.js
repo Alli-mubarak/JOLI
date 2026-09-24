37a43d0256e0d8168c1a3b87860e98e02ba02159
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-24 21:12:00 +0100

    Implement getReqTime function for date formatting
    Added a function to calculate and format the time difference from a given date.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -36,6 +36,39 @@ let postsContainer = document.getElementById("posts");
 let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
 let scrollPosition = 0;
 
+//function that calculates and reconfigures time/date 
+function getReqTime(reqTime){
+const targetDate = new Date(reqTime);
+const currentDate = new Date();
+
+const msDifference = currentDate - targetDate;
+const sDifference = msDifference / 1000;
+const mDifference = msDifference / (1000 * 60);
+const hDifference = msDifference / (1000 * 60 * 60);
+const dDifference = msDifference / (1000 * 60 * 60 * 24);
+const wDifference = msDifference / (1000 * 60 * 60 * 24 * 7);
+const mtDifference = msDifference / (1000 * 60 * 60 * 24 * 30 * 12);
+
+if(mtDifference > 1){
+const formattedDate = new Intl.DateTimeFormat('en-US', {
+  month: '2-digit',
+  day: '2-digit'
+}).format(targetDate);
+return formattedDate; 
+}else if (wDifference > 1){
+return `${Math.floor(wDifference)}w`;
+}else if (dDifference > 1){
+return `${Math.floor(dDifference)}d`;
+}else if (hDifference > 1){
+return `${Math.floor(hDifference)}h`;
+}else if (mDifference > 1){
+return `${Math.floor(mDifference)}m`;
+}else {
+return `${Math.floor(sDifference)}s`;
+}
+
+}
+
 function disableScrolling(){
   scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   document.body.style.position = 'fixed';
