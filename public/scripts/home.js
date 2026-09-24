2e397dc8832bef1d77046da7117b1622abb3f8fe
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-24 21:10:28 +0100

    Update home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -167,32 +167,6 @@ postsContainer.innerHTML += postCard;
 
 }
 
-//function that calculates the time the post was created compared to the current time
-function getPostTime(postTime){
-const targetDate = new Date(postTime);
-const currentDate = new Date();
-
-const msDifference = currentDate - targetDate;
-const sDifference = msDifference / 1000;
-const mDifference = msDifference / (1000 * 60);
-const hDifference = msDifference / (1000 * 60 * 60);
-const dDifference = msDifference / (1000 * 60 * 60 * 24);
-const mtDifference = msDifference / (1000 * 60 * 60 * 24 * 12);
-
-if(mtDifference > 1){
-return `${Math.floor(mtDifference)}M`; 
-}else if (dDifference > 1){
-return `${Math.floor(dDifference)}d`;
-}else if (hDifference > 1){
-return `${Math.floor(hDifference)}h`;
-}else if (mDifference > 1){
-return `${Math.floor(mDifference)}m`;
-}else {
-return `${Math.floor(sDifference)}s`;
-}
-
-}
-
 function sortImages(images){
       let result = "";
       images.forEach(imgLink=>{
