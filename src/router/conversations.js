37f1f5f6f5dd4779118df53716a9d16a176ec2f3
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-25 22:26:17 +0100

    Implement conversation creation API endpoint
    Add API endpoint for creating conversations with user validation and error handling.

--- src/router/conversations.js
+++ src/router/conversations.js
@@ -64,4 +64,43 @@ router.get('/user/conversations', checkSession,  async (req, res) => {
     }
 });
 
+// conversation creation api
+router.post('/v1/create-conversation', checkSession, async (req, res) => {
+  try{
+  if (!req.isAuthenticated() && !req.user){
+   return  res.status(400).json({error: 'You need to log in first!'});
+  }
+  
+  const userId = req.user.id;
+
+  if (!userId) {
+    return res.status(400).json({ error: 'user id is required' });
+  }
+  const { friend_id, friend_username, friend_pic, last_message} = req.body;
+  if (!friend_id || !friend_username || !friend_pic || !last_message) {
+    return res.status(400).json({ error: 'one or more information is missing' });
+  }
+    
+const queryText = `
+  INSERT INTO conversations (user_id, friend_id, friend_username, friend_pic, last_message)
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;
+`;
+    const values = [userId, friend_id, friend_username, friend_pic, last_message];
+    
+
+    const result = await pool.query(queryText, values);
+
+    const newConversation = result.rows[0];
+    console.log('Conversation successfully created!');
+    console.log(newConversation);
+    return res.status(201).json(newConversation);
+
+  } catch (error) {
+    console.error('Error creating conversation:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+      
+
 export default router;
