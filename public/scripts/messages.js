8a06650d1e48ca7cb81ce9e6841f852462cd715c
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2026-09-26 11:00:30 +0100

    Allow messaging self and log user details
    Removed self-message restriction and added console log for user details.

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -403,11 +403,9 @@ cFormInput.oninput = () => {
 
 async function sendMessage(id){
   try{
-  if(currentUserId === id) {
-    alert("you cannot message yourself");
-    console.log(fPic, fUsername);
-    return;
-  }
+  
+    console.log(fPic, fUsername, id);
+    
   const hasConversation = conversations.find(c => c.user_id === id || c.friend_id === id);
   if(conversations && !hasConversation){
     try{
